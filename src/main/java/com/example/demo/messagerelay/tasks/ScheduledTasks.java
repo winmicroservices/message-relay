28d87027d729eab34add681944d1f00fd96e6d39
William Polinchak <william.polinchak@windstream.com>
2023-01-19 14:43:03 -0500

    fix: #1 ScheduleTasks added.

---
+++ src/main/java/com/example/demo/messagerelay/tasks/ScheduledTasks.java
@@ -0,0 +1,21 @@
+package com.example.demo.messagerelay.tasks;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Component
+@Slf4j
+public class ScheduledTasks {
+
+	private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+
+	@Scheduled(fixedRate = 5000)
+	public void reportCurrentTime() {
+		log.info("The time is now {}", dateFormat.format(new Date()));
+	}
+}
