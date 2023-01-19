7bcb796e20fbac9350909376328c8ee9023d4782
William Polinchak <william.polinchak@windstream.com>
2023-01-19 15:33:03 -0500

    fix: #1 Adding event repository class.

---
+++ src/main/java/com/example/demo/messagerelay/repository/EventRepository.java
@@ -0,0 +1,12 @@
+package com.example.demo.messagerelay.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.demo.messagerelay.entity.Event;
+
+/**
+* Dao for the Event table.
+*/
+public interface EventRepository extends JpaRepository<Event, Long> {
+
+}
