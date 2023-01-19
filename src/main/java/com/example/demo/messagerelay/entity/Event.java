a44395d0528a4a35ac3e5da882c6a6372b0d3a5b
William Polinchak <william.polinchak@windstream.com>
2023-01-19 15:28:58 -0500

    fix: #1 Adding the event entity and jackson library.

---
+++ src/main/java/com/example/demo/messagerelay/entity/Event.java
@@ -0,0 +1,79 @@
+package com.example.demo.messagerelay.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+/**
+* This entity is used to collect the changes to the customer.  
+* The records will be streamed to an event queue.
+*/
+@Table(name = "event")
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+@Entity
+public class Event {
+
+    /**
+    * Primary key.
+    */
+    @Id
+    @Column(name="event_id")
+    @GeneratedValue(strategy = GenerationType.AUTO) 
+    private long eventId;
+
+    /**
+    * Used to track the state of the customer.
+    */
+    @Column(name = "action")
+    private String state;
+
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name="msg_sent")
+    private Boolean messageSent;
+
+    private String payload;
+
+    
+    public Event() {
+        
+    }
+    
+    public Event(String state, Long id, String payload) {
+        this.state = state;
+        this.id = id;
+        this.payload = payload;
+    }
+    
+    public String getState() {
+        return this.state;
+    }
+    
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public Boolean getMessageSent() {
+        return this.messageSent;
+    }
+    
+    public void setMessageSent(Boolean messageSent) {
+        this.messageSent = messageSent;
+    }
+    
+    public String getPayload() {
+        return this.payload;
+    }
+    
+    public void setPayload(String payload) {
+        this.payload = payload;
+    }
+}
