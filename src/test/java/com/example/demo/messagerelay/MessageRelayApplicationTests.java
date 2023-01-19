ceb6f90a5e2a16231a62535d4f4f14d5d52c757a
William Polinchak <william.polinchak@windstream.com>
2023-01-19 13:39:25 -0500

    Initial commit.

---
+++ src/test/java/com/example/demo/messagerelay/MessageRelayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo.messagerelay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessageRelayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
