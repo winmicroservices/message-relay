868b8dc2004c89026121c286f321e1d49b842dac
William Polinchak <polinchw@gmail.com>
2023-01-25 16:59:54 -0500

    fix: Adding info to Kafka #5

--- src/main/java/com/example/demo/messagerelay/component/kafka/TopicProducer.java
+++ src/main/java/com/example/demo/messagerelay/component/kafka/TopicProducer.java
@@ -3,6 +3,9 @@ package com.example.demo.messagerelay.component.kafka;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Component;
 
 import lombok.extern.slf4j.Slf4j;
@@ -24,9 +27,17 @@ public class TopicProducer {
      * Sends a create order message to a Kafka topic that tracks new customers.
      * @param message The Customer object in json format.
      */
-    public void send(String message){
+    public void send(String message, Long eventId, Long id, String action){
         log.info("Payload message: {}", message);
-        kafkaTemplate.send(topicName, message);
+        Message<String> m = MessageBuilder
+        .withPayload(message)
+        .setHeader(KafkaHeaders.TOPIC, topicName)
+        .setHeader("eventId",eventId.toString())
+        .setHeader("id",id.toString())
+        .setHeader("action",action)
+        .setHeader("X-Custom-Header", "Sending Custom Header with Spring Kafka")
+        .build();
+        kafkaTemplate.send(m);
     }
 
 }
